525cbfd7327d4c083ffe63ae5c01fee5705b6e54
Vlad Andrei Rosoiu <rosoiuandreivlad@gmail.com>
2020-07-15 21:57:36 +0300

    Setting up the game 0.1 (validating answer and creating loop for answering)

--- src/main/java/ro/jademy/whowantstobeamillionaire/WhoWantsToBeAMillionaire.java
+++ src/main/java/ro/jademy/whowantstobeamillionaire/WhoWantsToBeAMillionaire.java
@@ -5,20 +5,32 @@ import java.util.ArrayList;
 public class WhoWantsToBeAMillionaire {
     ArrayList<Question> questions;
     Player player;
+    int questionNumber=0;
 
     public WhoWantsToBeAMillionaire(ArrayList<Question> questions, Player player){
         this.questions=questions;
         this.player=player;
     }
 
-    public void welcomeMessage(){
-        System.out.println("Welcome to the greatest game in history!");
-        System.out.println("Who Wants To Be a Millionaire?!");
-        System.out.println("Rules here ..... ...... ...... ..");
+    public void printQuestion(){
+        System.out.println(questions.get(questionNumber).getQuestion());
     }
 
-    public boolean gameOn(){
-        return player.getTries()==1;
+    public void printAnswers(){
+        System.out.println(questions.get(questionNumber).getAnswer1());
+        System.out.println(questions.get(questionNumber).getAnswer2());
+        System.out.println(questions.get(questionNumber).getAnswer3());
+        System.out.println(questions.get(questionNumber).getAnswer4());
+    }
+
+    public void validateAnswer(String answer){
+        if (answer.equalsIgnoreCase(questions.get(questionNumber).getCorrectAnswer())){
+            System.out.println("Correct Answer!");
+            questionNumber++;
+        }else {
+            System.out.println("Wrong!");
+            player.setPlayerWrong(true);
+        }
     }
 
 }
